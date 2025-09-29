bae57a2cf2e1cc4c8979513c5888e6e02011519a
michele <mikdr51@gmail.com>
2025-09-29 15:42:08 +0200

    test user

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import bcrypt
 
 def get_connection():
     conn = mysql.connector.connect(
@@ -9,3 +10,24 @@ def get_connection():
         autocommit=True   # evita lock se ti dimentichi il commit
     )
     return conn
+
+def create_user():
+    username = "utente"
+    password_plain = "password"
+
+    # genera l'hash bcrypt (salt automatico)
+    hashed = bcrypt.hashpw(password_plain.encode(), bcrypt.gensalt())
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO utenti (Nome, Password) VALUES (%s, %s)"
+    cursor.execute(insert_query, (username, hashed.decode("utf-8")))
+
+    cursor.close()
+    conn.close()
+
+    print(f"✅ Utente '{username}' creato con successo")
+
+if __name__ == "__main__":
+    create_user()
